0266f036fbc5dd2479b00df3887f9a1f4ca30973
albert-zhong <albertzhong@hotmail.com>
2020-03-07 20:10:05 -0800

    Add radius searching feature

--- ebay.js
+++ ebay.js
@@ -1,15 +1,7 @@
 const baseURL = 'https://onyx-elevator-270422.appspot.com/';
-const parameters = {
-    'OPERATION-NAME': 'findItemsByKeywords',
-    'SERVICE-VERSION': '1.0.0',
-    'SECURITY-APPNAME': 'ebayhack-ecoBay-PRD-669e8f5f8-11393d83',
-    'GLOBAL-ID': 'EBAY-US',
-    'RESPONSE-DATA-FORMAT': 'JSON',
-    'keywords': 'batman',
-    'results': 3,
-};
 
-function getURL(baseURL, parameters) {
+
+function getURL(parameters) {
     let url = new URL(baseURL);
     for (let [key, value] of Object.entries(parameters)) {
         url.searchParams.append(key, value);
@@ -17,13 +9,23 @@ function getURL(baseURL, parameters) {
     return url.href;
 }
 
-function getData(url) {
-    fetch(url)
-        .then((response) => (response.json()))
-        .then((json) => (console.log(json)));
+function getJSON(url) {
+    console.log(url);
+    return fetch(url).then((response) => (response.json()))
 }
 
-function findItemsByKeywords(keyword, results) {
+// Searchs eBay for items that match the given keyword. Returns these in an
+// array of objects.
+//
+// REQUIRED PARAMS:
+// keyword (string): the keywords to search for items with. this can be multiple
+// words, just put a space between them in a single string
+// results (integer): number of results to return
+// 
+// OPTIONAL PARAMS:
+// postalCode (integer): the postal code of the buyer
+// maxRadius (integer): the maximum radius around the postalCode to search for
+function findItemsByKeywordsAndRadius(keyword, results, postalCode, maxRadius) {
     let parameters = {
         'OPERATION-NAME': 'findItemsByKeywords',
         'SERVICE-VERSION': '1.0.0',
@@ -33,6 +35,52 @@ function findItemsByKeywords(keyword, results) {
         'keywords': keyword,
         'paginationInput.entriesPerPage': results,
     }
-    let url = getURL(baseURL, parameters);
-    return getData(url);
+
+    if (postalCode && maxRadius) {
+        parameters['buyerPostalCode'] = postalCode;
+        parameters['itemFilter(0).name'] = 'LocalSearchOnly';
+        parameters['itemFilter(0).value'] = true;
+        parameters['itemFilter(1).name'] = 'MaxDistance';
+        parameters['itemFilter(1).value'] = 100;
+    }
+
+    let url = getURL(parameters);
+    console.log(url);
+    return getJSON(url).then((raw) => {
+        let rawItems = raw.findItemsByKeywordsResponse[0].searchResult[0].item;
+        let newItems = [];
+        for (let item of rawItems) {
+            newItems.push({
+                'title': item.title[0],
+                'imageURL': item.galleryURL[0],
+                'location': item.location[0],
+                'price': parseInt(item.sellingStatus[0].currentPrice[0].__value__),
+                'itemURL': item.viewItemURL[0],
+            });
+        }
+        return newItems;
+    });
 }
+
+function findItemsByKeywords(keyword, results) {
+    return findItemsByKeywordsAndRadius(keyword, results, null, null);
+}
+
+/*
+EXAMPLE: prints out the top 5 results for 'shoes' on eBay:
+
+findItemsByKeywords('shoes', 5).then((items) => {
+    for (let item of items) {
+        console.log(item);
+    }
+});
+
+EXAMPLE: prints out the top 5 results for 'batman', with a radius of 10 miles
+around the Zip Code 98195.
+
+findItemsByKeywordsAndRadius('batman', 5, 98195, 10).then((items) => {
+    for (let item of items) {
+        console.log(item);
+    }
+});
+*/
