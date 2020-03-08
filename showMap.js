c8a765ab0cbadcbe483f60d73b929057b6ff2cb6
deehrlic <deehrlic@ucsc.edu>
2020-03-07 16:06:16 -0800

    esri??

---
+++ showMap.js
@@ -0,0 +1,32 @@
+function getGeo(){
+   if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+          })};
+  return pos;
+}
+
+require([
+      "esri/Map",
+      "esri/views/MapView"
+    ],
+
+
+
+    function(Map, MapView) {
+
+    var map = new Map({
+      basemap: "topo-vector"
+    });
+
+
+    var view = new MapView({
+      container: "viewDiv",
+      map: map,
+      center: [pos.lat, pos.lng], // longitude, latitude
+      zoom: 13
+    });
+  });
