298c41ebdc11b69979bca3fa93c125d975b4ef58
Bryan Lim <bryanjlim@outlook.com>
2020-03-07 17:45:54 -0800

    Improved product recognition

--- languageProcessing.js
+++ languageProcessing.js
@@ -1,69 +1,58 @@
-document.addEventListener(
-  "DOMContentLoaded",
-  function() {
-    var checkPageButton = document.getElementById("checkPage");
-    checkPageButton.addEventListener(
-      "click",
-      function() {
-        chrome.tabs.getSelected(null, function(tab) {
-          analyzeText(chrome);
-        });
-      },
-      false
-    );
-  },
-  false
-);
+function analyzeText(text) {
+  return new Promise((resolve, reject) => {
+    const requestUrl = [
+      "https://language.googleapis.com/v1/documents:analyzeEntities?key=",
+      "AIzaSyADeFluX6JmUaFcjWewHHd80Tyl-3xAJw8"
+    ].join("");
 
-function analyzeText() {
-  const requestUrl = [
-    "https://language.googleapis.com/v1/documents:analyzeEntities?key=",
-    "AIzaSyADeFluX6JmUaFcjWewHHd80Tyl-3xAJw8"
-  ].join("");
-
-  const data = {
-    document: {
-      type: "PLAIN_TEXT",
-      language: "EN",
-      content:
-        "Google, headquartered in Mountain View (1600 Amphitheatre Pkwy, Mountain View, CA 940430), unveiled the new Android phone for $799 at the Consumer Electronic Show. Sundar Pichai said in his keynote that users love their new Android phones."
-    }
-  };
-
-  fetch(requestUrl, {
-    method: "post",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  }).then(fetched => {
-    fetched.json().then(res => {
-      // const printableResult = JSON.stringify(res);
-      let org;
-      let item;
-      let i = 0;
-      while (
-        i < res["entities"].length &&
-        (typeof org === "undefined" || typeof item === "undefined")
-      ) {
-        if (
-          typeof org === "undefined" &&
-          res["entities"][i].type === "ORGANIZATION"
-        ) {
-          org = res["entities"][i].name;
-        }
-        if (
-          typeof item === "undefined" &&
-          res["entities"][i].type === "CONSUMER_GOOD"
-        ) {
-          item = res["entities"][i].name;
-        }
-        i++;
+    const data = {
+      document: {
+        type: "PLAIN_TEXT",
+        language: "EN",
+        content: text
       }
-      // TODO: Use org and item
-      document.body.innerHTML += "<h1>" + org + "</h1>";
-      document.body.innerHTML += "<h1>" + item + "</h1>";
+    };
+
+    fetch(requestUrl, {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    }).then(fetched => {
+      fetched.json().then(res => {
+        // console.log(res);
+        // const printableResult = JSON.stringify(res);
+        let org;
+        let item;
+        let i = 0;
+        while (
+          i < res["entities"].length &&
+          (typeof org === "undefined" || typeof item === "undefined")
+        ) {
+          if (
+            typeof org === "undefined" &&
+            res["entities"][i].type === "ORGANIZATION"
+          ) {
+            org = res["entities"][i].name;
+          }
+          if (
+            typeof item === "undefined" &&
+            (res["entities"][i].type === "CONSUMER_GOOD" ||
+              res["entities"][i].type === "LOCATION" ||
+              res["entities"][i].type === "WORK_OF_ART") &&
+            !res["entities"][i].name.toLowerCase().includes("product") &&
+            (res["entities"][i].mentions.length > 1 ||
+              res["entities"][i].salience > 0.15)
+          ) {
+            item = res["entities"][i].name;
+          }
+          i++;
+        }
+        // Only using the item right now, could use org too
+        resolve(item);
+      });
     });
   });
 }
--- popup.js
+++ popup.js
@@ -1,20 +1,39 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
+document.addEventListener(
+  "DOMContentLoaded",
+  function() {
+    var checkPageButton = document.getElementById("checkPage");
+    checkPageButton.addEventListener(
+      "click",
+      function() {
+        chrome.tabs.executeScript(
+          null,
+          {
+            file: "getPagesSource.js"
+          },
+          function() {
+            // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+            if (chrome.runtime.lastError) {
+              message.innerText =
+                "There was an error injecting script : \n" +
+                chrome.runtime.lastError.message;
+            }
+          }
+        );
+      },
+      false
+    );
+  },
+  false
+);
 
-    chrome.tabs.getSelected(null, function(tab) {
-      d = document;
-
-      var f = d.createElement('form');
-      f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
-    });
-  }, false);
-}, false);
+chrome.runtime.onMessage.addListener(function(request, sender) {
+  if (request.action == "getSource") {
+    if (request.source.found) {
+      message.innerText = request.source.val;
+    } else {
+      analyzeText(request.source.val).then(processedItem => {
+        message.innerText = processedItem;
+      });
+    }
+  }
+});
